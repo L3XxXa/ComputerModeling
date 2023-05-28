7e5b9ec27e5eb496ec51415bff119cdee02068a2
L3XxXa <alexkkk@inbox.ru>
2023-05-28 17:41:49 +0700

    Writing to CSV filr

--- algo.py
+++ algo.py
@@ -1,9 +1,26 @@
+import csv
+
 # Необходимо заполнить список массами частей самолета
-plane_masses = []
+plane_masses = [
+    3, 3, 3, 3, 3, 4, 4, 4, 
+    4, 4, 4, 4, 4, 4, 5, 5, 
+    5, 5, 6, 3, 5, 5, 5, 5, 
+    5, 5, 5, 5, 5, 5, 5, 5, 
+    5, 5, 4, 4, 3, 3, 3, 3, 
+    3, 3, 3, 3, 3, 3, 20, 20, 
+    20, 20, 20, 20, 20, 20, 20, 
+    20, 2, 2, 2, 2, 2, 2, 2, 2, 
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 
+    2, 2, 5, 5, 5, 5, 5, 5, 5, 
+    5, 5, 5, 5, 5, 5, 5, 5, 5]
 # Позиция, в которую мы будем добавлять массу
-position = 0
+position = 19
 # Координата фокуса
-x_f = 0
+x_f = 46
+
+f = open("./file.csv", "w")
+writer = csv.writer(f)
 
 def find_center_masses() -> int:
     denom = sum(plane_masses)
@@ -22,11 +39,16 @@ def find_mass() -> int:
             return plane_masses[position]
         plane_masses[position] += 0.001
         x_c = find_center_masses()
-
+        write_to_file_data(plane_masses[position], x_c, x_f)
     return plane_masses[position] - initial_mass_positions
+
+def write_to_file_data(current_mass: int, xc: float, xf: float()):
+    data = [round(current_mass, 4), round(xc, 3), xf]
+    writer.writerow(data)
 
 
 def main():
+    writer.writerow([f"current mass in position {position}", "x_c", "x_f"])
     needed = find_mass()
     print(f"Needed about {needed} to place in position {position}")
 
